5434eac890cee381a4b655cb2d0820cb173d1bec
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-15 10:45:04 +0300

    Task23 done

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите целое число: ");
+        int Num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= Num; i++)
+        {
+            if (i != Num)
+            {
+                Console.Write(Math.Pow(i, 3) + ", ");
+            }
+            else
+            {
+                Console.Write(Math.Pow(i, 3));
+            }
+        }
+    }
+}
