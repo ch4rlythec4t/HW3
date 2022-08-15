6b26e7cc48ff4a2d669fb36918c632b28e2146f8
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-15 09:29:19 +0300

    Task19 done

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите целое число: ");
+        string? Num = Console.ReadLine();
+        int NumLength = Num!.Length;
+        bool Palindrom = true;
+        for (int index = 0; index < (NumLength - 1)/2; index++)
+        {
+            if (Num[index] == Num[NumLength - 1 - index])
+            {
+                Palindrom = true;
+            }
+            else
+            {
+                Palindrom = false;
+                break;
+            }
+        }
+
+        if (Palindrom == true)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+}
